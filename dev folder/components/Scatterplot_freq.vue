88ba97d71ee37c7fd46254a5adcbf6434d1e3ce6
FrancescoDiCursi <f.dicursi@studenti.unipi.it>
2022-10-11 15:36:58 +0200

    first page completed

--- dev folder/components/Scatterplot_freq.vue
+++ dev folder/components/Scatterplot_freq.vue
@@ -69,7 +69,8 @@ export default {
         "even",
         "lest",
         "now when",
-      ]
+      ],
+      colors:[]
     };
   },
   mounted() {
@@ -90,6 +91,46 @@ export default {
 
     this.y_ = temp_vals.map((d) => d[1]);
     console.log(this.x_, this.y_);
+    //colors
+    var coords_=[]
+    var subords_=[]
+
+    for(let i=0;i<this.x_.length;i++){
+      var coord_counter=0
+      for(let j=0;j<this.coord_conj.length;j++){
+        if (this.coord_conj[j]==this.x_[i].toLowerCase()){
+         coord_counter+=1
+        }
+      }
+
+        var subord_counter=0
+        for(let j=0;j<this.subord_conj.length;j++){
+          if(this.subord_conj[j]==this.x_[i].toLowerCase()){
+            subord_counter+=1
+          }
+        }
+      
+
+      coords_.push(coord_counter)
+      subords_.push(-Math.abs(subord_counter))
+
+      var colors_temp = coords_.map((d, i) => d + subords_[i]);
+
+      var color_vals=[]
+      //normalize
+      for(let i=0;i<colors_temp.length;i++){
+        if (colors_temp[i]==0){
+          color_vals.push('neutral')
+        }else if(colors_temp[i]>0){
+          color_vals.push('coord')
+        }else if(colors_temp[i]<0){
+          color_vals.push('subord')
+        }
+      }
+    }
+
+    console.log(color_vals)
+    this.colors=color_vals
   },
   methods: {
     check_value() {
@@ -101,34 +142,98 @@ export default {
         if (this.filt_type == this.filt_options[1]) {
           var filt_x = this.x_.slice(0, filt_value);
           var filt_y = this.y_.slice(0, filt_value);
+          var filt_colors=this.colors.slice(0,filt_value)
         } else if (this.filt_type == this.filt_options[0]) {
           var filt_x = [];
           var filt_y = [];
+          var filt_colors=[]
           for (let i = 0; i < this.x_.length; i++) {
             if (this.y_[i] <= filt_value) {
               filt_x.push(this.x_[i]);
               filt_y.push(this.y_[i]);
+              filt_colors.push(this.colors[i])
             }
           }
         }
       } else if(this.start_flag=true){
         var filt_x = this.x_;
         var filt_y = this.y_;
+        var filt_colors=this.colors
       }
+var traces=[]
+      var coord_filt=filt_colors.map(function(d,i){
+        if(d=='coord'){
+          return [filt_x[i],filt_y[i],'Coord','red']
+        }
+        else{
+          return ''
+        }
+      })
+      var subord_filt=filt_colors.map(function(d,i){
+        if(d=='subord'){
+          return [filt_x[i],filt_y[i],'Subord','blue']
+        }else{
+          return ''
+        }
+      })
+      var neutral_filt= filt_colors.map(function(d,i){
+        if(d=='neutral'){
+          return [filt_x[i],filt_y[i],'Neutral','grey']
+        }else{
+          return ''
+        }
+      })
+
+      coord_filt=coord_filt.filter(function(d){
+        if(d!=''){
+          return d
+        }})
+
+        subord_filt=subord_filt.filter(function(d){
+        if(d!=''){
+          return d
+        }})
+
+        neutral_filt=neutral_filt.filter(function(d){
+        if(d!=''){
+          return d
+        }})
+
+      var temp_data=[coord_filt,subord_filt,neutral_filt]
+      var line_colors=['red','blue','grey']
+      var trace_names=['Coord','Subord','Neutral']
+
+      console.log(temp_data,coord_filt,subord_filt,neutral_filt)
+      for(let i=0;i<3;i++){
       var trace = {
-        x: filt_x,
-        y: filt_y,
+        name:trace_names[i],
+        x: temp_data[i].map(d=>d[0]),
+        y: temp_data[i].map(d=>d[1]),
         mode: "lines+markers",
         type: "scatter",
+        marker:{
+          color:temp_data[i].map(d=>d[3])
+        },
+        line:{
+          color:line_colors[i]
+        }
       };
-      var layout = {};
-      Plotly.newPlot("freq_plot_single", [trace], layout);
+      traces.push(trace)
+    }
+      console.log(traces)
+      var layout = {
+        yaxis:{
+          categoryorder:'total descending'
+        }
+      };
+      Plotly.newPlot("freq_plot_single", traces, layout);
       this.start_flag = false;
+    
     },
   },
 
   watch: {
-    y_: function () {
+    colors: function () {
       this.draw_scatter();
     },
   },
